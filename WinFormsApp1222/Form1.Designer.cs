e1ce2747769f5f55a12ea16bd5f448852896203d
ptn1411 <zvw180ptn1411@gmai.com>
2022-04-26 15:58:36 +0700

    first commit

---
+++ WinFormsApp1222/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace WinFormsApp1222
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(452, 407);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 66);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Bat dau";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 23);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(690, 225);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 407);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(142, 66);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Thoat";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox2.Location = new System.Drawing.Point(22, 323);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(123, 34);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(59, 280);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 28);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "User";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox3.Location = new System.Drawing.Point(180, 323);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(123, 34);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(206, 280);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 28);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Profile";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(375, 323);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(291, 68);
+            this.richTextBox2.TabIndex = 9;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(486, 280);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 28);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Caption";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(719, 500);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Upload Video";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private RichTextBox richTextBox1;
+        private Button button2;
+        private TextBox textBox2;
+        private Label label1;
+        private TextBox textBox3;
+        private Label label2;
+        private RichTextBox richTextBox2;
+        private Label label3;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
---
+++ WinFormsApp1222/Form1.cs
@@ -0,0 +1,321 @@
+﻿using AutoItX3Lib;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace WinFormsApp1222
+{
+
+    public partial class Form1 : Form
+    {
+        
+        IWebDriver chromeDriver;
+        AutoItX3 autoIT;
+        String? userDataDir = null;
+        String? profileDirectory = null;
+        String? forder = null;
+        String? caption = null;
+        String ip = "45.140.13.119:9132";
+        String username = "username";
+        String pass = "password";
+        String? filePathJson = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            try
+            {
+              
+                string appPath = Application.StartupPath;
+                string filePath = "config.json";
+                string fullpath = Path.Combine(appPath, filePath);
+                filePathJson = fullpath;
+                string json = File.ReadAllText(fullpath);
+                dynamic jsonObj = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+                string user = jsonObj["user"];
+                string profile = jsonObj["profile"];
+                string caption_json = jsonObj["caption"];
+                
+                if (user != null && profile != null && caption_json != null)
+                {
+                    textBox2.Text = user;
+                    textBox3.Text = profile;
+                    richTextBox2.Text = caption_json;
+                    string LOGIN_DATA_PATH= "\\..\\Local\\Google\\Chrome\\User Data\\";
+                    var appdata = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);// APPDATA
+                    var p = Path.GetFullPath(appdata + LOGIN_DATA_PATH);
+                    userDataDir = p;
+
+                    profileDirectory = profile;
+
+                    caption = caption_json;
+                }
+                else
+                {
+                    MessageBox.Show("Please check config.json file");
+                    this.Close();
+                }
+                 
+
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(""+ex);
+                EditApp();
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+               
+
+                var Lines = FormatUid();
+                if (Lines != null)
+                {
+                    foreach (string Line in Lines)
+                    {
+                        forder = Line;
+                        EditFolderOpen();
+                        var driverService = ChromeDriverService.CreateDefaultService();
+
+                        driverService.HideCommandPromptWindow = false;
+                        ChromeOptions option = new ChromeOptions();
+                        autoIT = new AutoItX3();
+                        option.AddArgument(@"user-data-dir=" + userDataDir);
+                        option.AddArgument("--profile-directory=" + profileDirectory);
+
+                        option.AddArgument("start-maximized");
+                        option.AddArgument("--disable-extensions");
+                        // option.AddArgument("headless");
+                        option.AddArgument("--no-sandbox");
+                        option.AddArgument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36");
+                        //option.AddArgument("--log-level=3");
+                        // option.AddArgument(string.Format("--proxy-server={0}", ip));
+                        chromeDriver = new ChromeDriver(driverService, option);
+                        chromeDriver.Url = "https://phamthanhnam.com/?utm_source=phamthanhnam.com&utm_medium=url_shortener";
+                        IJavaScriptExecutor js = (IJavaScriptExecutor)chromeDriver;
+                        js.ExecuteScript("javascript:(() => {function pageScroll() {window.scrollBy(0, 1000);window.scrollTo({ top: 0, behavior: 'smooth' });setTimeout(pageScroll, 2000);}pageScroll();})();");
+                        Thread.Sleep(TimeSpan.FromSeconds(5));
+                        chromeDriver.Url = "https://www.tiktok.com/upload";
+                        chromeDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(6);
+
+                        var firstResult = chromeDriver.FindElement(By.TagName("iframe"));
+
+                        chromeDriver.SwitchTo().Frame(firstResult);
+
+                        foreach (string file in Directory.EnumerateFiles(Line, "*.mp4"))
+                        {
+                            string file_path = Path.GetFileName(file);
+                            UploadFile(file_path);
+                        }
+
+                        chromeDriver.Dispose();
+                        chromeDriver.Quit();
+                    };
+                }
+                else
+                {
+                    MessageBox.Show("Please check config.json file");
+                    this.Close();
+                }
+          
+            }
+            catch (Exception ex)
+            {
+               //
+               //MessageBox.Show("" + ex);                
+                EditApp();
+            }
+
+        }
+        public void OpenNewTab(string url)
+        {
+            IJavaScriptExecutor js = (IJavaScriptExecutor)chromeDriver;
+            js.ExecuteScript(string.Format("window.open('{0}', '_blank');", url));
+        }
+
+        public string CoventUidToUrl(string uid)
+        {
+            string url = "https://www.facebook.com/groups/" + uid + "/about";
+            return url;
+        }
+        public Array FormatUid()
+        {
+            String text = richTextBox1.Text.ToString();
+
+            if (text != null)
+            {
+                string[] lines = text.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+                return lines;
+            }
+            else
+            {
+                return null;
+            }
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        public void EditFolderOpen()
+        {
+            try
+            {
+                string json = File.ReadAllText(userDataDir + profileDirectory + "\\Preferences");
+                dynamic jsonObj = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+                jsonObj["selectfile"]["last_directory"] = forder;
+                string output = Newtonsoft.Json.JsonConvert.SerializeObject(jsonObj, Newtonsoft.Json.Formatting.Indented);
+                File.WriteAllText(userDataDir + profileDirectory + "/Preferences", output);
+            }
+            catch (Exception ex)
+            {
+              //  MessageBox.Show("" + ex);
+                EditApp();
+            }
+
+
+        }
+        public void UploadFile(string file_path)
+        {
+            try
+            {
+                chromeDriver.FindElement(By.ClassName("css-ku5jwq")).Click();
+                IJavaScriptExecutor js = (IJavaScriptExecutor)chromeDriver;
+                js.ExecuteScript("var element = document.getElementsByClassName('public-DraftStyleDefault-block')[0].children[0].getAttribute('data-offset-key');");
+                var caption_elem = chromeDriver.FindElement(By.ClassName("public-DraftStyleDefault-block"));
+                caption_elem.SendKeys(caption);
+                autoIT.WinActivate("Open");
+                autoIT.ControlFocus("Open", "", "Edit1");
+                Thread.Sleep(TimeSpan.FromSeconds(2));
+                autoIT.Send(file_path);
+                Thread.Sleep(TimeSpan.FromSeconds(2));
+
+                autoIT.Send("{ENTER}");
+                Thread.Sleep(TimeSpan.FromSeconds(15));
+
+                chromeDriver.FindElement(By.ClassName("css-15xm9lp")).Click();
+
+                Thread.Sleep(TimeSpan.FromSeconds(7));
+                chromeDriver.FindElement(By.ClassName("modal-btn")).Click();
+                Thread.Sleep(TimeSpan.FromSeconds(2));
+            }
+            catch (Exception ex)
+            {
+              //  MessageBox.Show("" + ex);
+                EditApp();
+            }
+          
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+            
+        {
+            EditApp();
+        }
+        public void EditApp() {
+
+            try
+            {
+             
+
+                Application.Exit();
+            }
+            catch (Exception e )
+            {
+                
+            }
+
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                string user = textBox2.Text;
+          
+                   
+
+                    string json = File.ReadAllText(filePathJson);
+                    dynamic jsonObj = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+                    jsonObj["user"] = user;
+                    string output = Newtonsoft.Json.JsonConvert.SerializeObject(jsonObj, Newtonsoft.Json.Formatting.Indented);
+
+                System.IO.File.WriteAllText(filePathJson, output);
+              
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show("" + ex);
+                EditApp();
+            }
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                string profile = textBox3.Text;
+                profileDirectory = textBox3.Text.ToString();
+
+                string json = File.ReadAllText(filePathJson);
+                dynamic jsonObj = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+                jsonObj["profile"] = profile;
+                string output = Newtonsoft.Json.JsonConvert.SerializeObject(jsonObj, Newtonsoft.Json.Formatting.Indented);
+                System.IO.File.WriteAllText(filePathJson, output);
+            }
+            catch (Exception ex)
+            {
+//MessageBox.Show("" + ex);
+                EditApp();
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                 
+                caption = richTextBox2.Text;
+
+                string json = File.ReadAllText(filePathJson);
+                dynamic jsonObj = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+                jsonObj["caption"] = caption;
+                string output = Newtonsoft.Json.JsonConvert.SerializeObject(jsonObj, Newtonsoft.Json.Formatting.Indented);
+                System.IO.File.WriteAllText(filePathJson, output);
+            }
+            catch (Exception ex)
+            {
+               // MessageBox.Show("" + ex);
+                EditApp();
+            }
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
